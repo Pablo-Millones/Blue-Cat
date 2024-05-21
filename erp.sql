3a4d9402d0a0bc7609a006cac113cc0108b2e985
Pablo Millones <154716081+Pablo-Millones@users.noreply.github.com>
2024-05-21 17:36:35 -0400

    Update erp.sql

--- erp.sql
+++ erp.sql
@@ -11,224 +11,91 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
 -- Base de datos: `erp`
---
-
 -- --------------------------------------------------------
 
---
 -- Estructura de tabla para la tabla `detalle_pedido`
---
-
 CREATE TABLE `detalle_pedido` (
-  `id_detalle_pedido` int NOT NULL,
+  `id_detalle_pedido` int NOT NULL AUTO_INCREMENT,
   `id_pedido` int NOT NULL,
   `id_producto` int NOT NULL,
   `cantidad_pedida` int NOT NULL,
-  `precio_total` int NOT NULL
+  `precio_total` int NOT NULL,
+  PRIMARY KEY (`id_detalle_pedido`),
+  KEY `id_pedido` (`id_pedido`),
+  KEY `id_producto` (`id_producto`),
+  CONSTRAINT `detalle_pedido_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `detalle_pedido_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `metodo_de_pago`
---
-
 CREATE TABLE `metodo_de_pago` (
-  `id_metodo_de_pago` int NOT NULL,
+  `id_metodo_de_pago` int NOT NULL AUTO_INCREMENT,
   `id_pedido` int NOT NULL,
   `nombre_metodo_pago` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `monto` int NOT NULL
+  `monto` int NOT NULL,
+  PRIMARY KEY (`id_metodo_de_pago`),
+  KEY `id_pedido` (`id_pedido`),
+  CONSTRAINT `metodo_de_pago_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `pedido`
---
-
 CREATE TABLE `pedido` (
-  `id_pedido` int NOT NULL,
+  `id_pedido` int NOT NULL AUTO_INCREMENT,
   `id_sesion` int NOT NULL,
   `precio_total` int NOT NULL,
   `pago_total` int NOT NULL,
   `diferencia` int NOT NULL,
-  `fecha` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `fecha` datetime NOT NULL,
+  PRIMARY KEY (`id_pedido`),
+  KEY `id_sesion` (`id_sesion`),
+  CONSTRAINT `pedido_ibfk_6` FOREIGN KEY (`id_sesion`) REFERENCES `sesion` (`id_sesion`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci AUTO_INCREMENT=229;
 
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `producto`
---
-
 CREATE TABLE `producto` (
-  `id_producto` int NOT NULL,
+  `id_producto` int NOT NULL AUTO_INCREMENT,
   `id_user` int NOT NULL,
   `nombre_producto` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `precio_venta` int DEFAULT NULL,
   `codigo_de_barras` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `cantidad` int DEFAULT NULL,
-  `categoria` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `categoria` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id_producto`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci AUTO_INCREMENT=4568;
 
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `sesion`
---
-
 CREATE TABLE `sesion` (
-  `id_sesion` int NOT NULL,
+  `id_sesion` int NOT NULL AUTO_INCREMENT,
   `id_user` int NOT NULL,
   `fecha_ingreso` varchar(50) NOT NULL,
   `monto_apertura` int DEFAULT NULL,
   `empleado` varchar(30) DEFAULT NULL,
-  `nota` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `nota` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id_sesion`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `sesion_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci AUTO_INCREMENT=60;
 
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `usuario`
---
-
 CREATE TABLE `usuario` (
-  `id_user` int NOT NULL,
+  `id_user` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(20) NOT NULL,
   `correo` varchar(50) NOT NULL,
   `password` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `validar_sesion` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `validar_sesion` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci AUTO_INCREMENT=4;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `detalle_pedido`
---
-ALTER TABLE `detalle_pedido`
-  ADD PRIMARY KEY (`id_detalle_pedido`),
-  ADD KEY `id_pedido` (`id_pedido`),
-  ADD KEY `id_producto` (`id_producto`);
-
---
--- Indices de la tabla `metodo_de_pago`
---
-ALTER TABLE `metodo_de_pago`
-  ADD PRIMARY KEY (`id_metodo_de_pago`),
-  ADD KEY `id_pedido` (`id_pedido`);
-
---
--- Indices de la tabla `pedido`
---
-ALTER TABLE `pedido`
-  ADD PRIMARY KEY (`id_pedido`),
-  ADD KEY `id_sesion` (`id_sesion`);
-
---
--- Indices de la tabla `producto`
---
-ALTER TABLE `producto`
-  ADD PRIMARY KEY (`id_producto`),
-  ADD KEY `id_user` (`id_user`);
-
---
--- Indices de la tabla `sesion`
---
-ALTER TABLE `sesion`
-  ADD PRIMARY KEY (`id_sesion`),
-  ADD KEY `id_user` (`id_user`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_user`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `detalle_pedido`
---
-ALTER TABLE `detalle_pedido`
-  MODIFY `id_detalle_pedido` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `metodo_de_pago`
---
-ALTER TABLE `metodo_de_pago`
-  MODIFY `id_metodo_de_pago` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `pedido`
---
-ALTER TABLE `pedido`
-  MODIFY `id_pedido` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=229;
-
---
--- AUTO_INCREMENT de la tabla `producto`
---
-ALTER TABLE `producto`
-  MODIFY `id_producto` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4568;
-
---
--- AUTO_INCREMENT de la tabla `sesion`
---
-ALTER TABLE `sesion`
-  MODIFY `id_sesion` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
-
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `detalle_pedido`
---
-ALTER TABLE `detalle_pedido`
-  ADD CONSTRAINT `detalle_pedido_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `detalle_pedido_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Filtros para la tabla `metodo_de_pago`
---
-ALTER TABLE `metodo_de_pago`
-  ADD CONSTRAINT `metodo_de_pago_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id_pedido`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Filtros para la tabla `pedido`
---
-ALTER TABLE `pedido`
-  ADD CONSTRAINT `pedido_ibfk_6` FOREIGN KEY (`id_sesion`) REFERENCES `sesion` (`id_sesion`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Filtros para la tabla `producto`
---
-ALTER TABLE `producto`
-  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Filtros para la tabla `sesion`
---
-ALTER TABLE `sesion`
-  ADD CONSTRAINT `sesion_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
